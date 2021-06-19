687d7b717e3e899f0ac55fcfb33df08981632e54
LuciferUZB <soloxiddinovazizbek@gmail.com>
2021-06-19 10:36:49 +0500

    Masala yechildi

---
+++ index.js
@@ -0,0 +1,116 @@
+// 1-masala
+var p;
+var a = 15;
+p = 4*15
+console.log("javob",p)
+
+
+// 2-masala
+var s;
+var a = 4;
+s = a**2
+console.log("javob",s)
+
+
+// 3-masala
+var s;
+var a =3; b =4;
+s = a*b
+
+var p;
+p = 2*(a+b)
+
+console.log("yuza",s)
+console.log("perimetr",p)
+
+
+// 4-masala
+const pi = 3.14
+var d = 7;
+var l = d*pi
+
+console.log("aylana yuzi",l)
+
+
+// 5-masala
+var a = 5
+var v = a**3
+var s = 6* a**2
+
+
+console.log("javob")
+
+// 6-masala
+var a = 3;
+var b = 4;
+var c = 7;
+
+
+v = a*b*c
+var s = 2 * (a*b + b*c + a*c) 
+
+
+console.log("javob")
+
+
+// 7-masala
+const pi = 3.14
+var r = 5;
+var l =2*pi*r
+var s = pi*r*2
+
+console.log("yuzi",s)
+console.log("uzunligi",l)
+
+
+// 8-masala
+var a = 12
+var b = 4
+var c = (a+b)/2
+
+
+console.log("javob")
+
+
+// 9-masala
+var n = 35
+var m = 10
+var yigindi = n-m
+console.log("yig`indi",yigindi)
+console.log(Math.sqrt(n-m))
+
+
+// 10-masala
+var n = 2
+var m = 4
+var yigindi = n+m
+var kopaytma = n*m
+var kvadratN = n**2
+var kvadratM = n**2
+
+console.log("yig`indi",yigindi)
+console.log("ko`paytma",kopaytma)
+
+
+// 11-masala
+var n = 8
+var m = 4
+var yigindi = n+m
+var kopaytma = n*m
+
+console.log("yig`indi",yigindi)
+console.log("ko`paytma",kopaytma)
+
+
+// 12-masala
+var a = 2
+var b = 4
+var c;
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+console.log("Side c =",c)
+
+var P;
+var P = a+b+c
+console.log("javob",P)
